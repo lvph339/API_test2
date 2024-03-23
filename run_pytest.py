38926898239d9b3d1f6359cb4b5378c72d6a8341
lvph339 <lvph339@163.com>
2024-03-23 16:11:34 +0800

    修改调用错误

--- run_pytest.py
+++ run_pytest.py
@@ -1,13 +1,13 @@
 # -*- coding:utf-8 -*-
 import os
 import pytest
-from utils.PublicUtils import send_mail
-from utils.PublicUtils import zip_file
+from utils.PublicUtils import PublicUtils
+
 
 
 
 if __name__ == '__main__':
     pytest.main(['-s','-v','./workplace/test_case_yaml_pytest.py','-n=4','--clean-alluredir', '--alluredir=./allure/allure-results'])  # -n=x,x代表启动进程数
     os.system('allure generate ./allure/allure-results -o ./allure/allure-report/ --clean')
-    zip_file('./allure')
-    send_mail('XXXXXXX','./allure.zip')
+    PublicUtils.zip_file('./allure')
+    PublicUtils.send_mail('XXXXXXX','./allure.zip')
